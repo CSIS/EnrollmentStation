b4ff91c7f8e0936cc58ee8b84243710cd021e486
Michael Bisbjerg <mike@csis.dk>
2015-05-18 10:28:16 +0200

    Refactoring

--- CertUtilities/EnrollmentStation/Code/CertificateUtilities.cs
+++ CertUtilities/EnrollmentStation/Code/CertificateUtilities.cs
@@ -8,17 +8,6 @@ using CERTENROLLLib;
 
 namespace EnrollmentStation.Code
 {
-    public enum RevokeReason
-    {
-        CRL_REASON_UNSPECIFIED = 0,
-        CRL_REASON_KEY_COMPROMISE = 1,
-        CRL_REASON_CA_COMPROMISE = 2,
-        CRL_REASON_AFFILIATION_CHANGED = 3,
-        CRL_REASON_SUPERSEDED = 4,
-        CRL_REASON_CESSATION_OF_OPERATION = 5,
-        CRL_REASON_CERTIFICATE_HOLD = 6
-    }
-
     public static class CertificateUtilities
     {
         private const int CC_UIPICKCONFIG = 0x1;
---
+++ CertUtilities/EnrollmentStation/Code/RevokeReason.cs
@@ -0,0 +1,13 @@
+namespace EnrollmentStation.Code
+{
+    public enum RevokeReason
+    {
+        CRL_REASON_UNSPECIFIED = 0,
+        CRL_REASON_KEY_COMPROMISE = 1,
+        CRL_REASON_CA_COMPROMISE = 2,
+        CRL_REASON_AFFILIATION_CHANGED = 3,
+        CRL_REASON_SUPERSEDED = 4,
+        CRL_REASON_CESSATION_OF_OPERATION = 5,
+        CRL_REASON_CERTIFICATE_HOLD = 6
+    }
+}
