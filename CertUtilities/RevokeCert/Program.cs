226aaf5a7a40afd85708d0f39acd05a2fb29a3ed
Michael Bisbjerg <mike@csis.dk>
2015-05-18 10:24:23 +0200

    Made RevokeCert handle both variations of the commandline

--- CertUtilities/RevokeCert/Program.cs
+++ CertUtilities/RevokeCert/Program.cs
@@ -8,24 +8,52 @@ using CERTADMINLib;
 #if WIN8_COMPILE
  using CERTCLILib;
 #else
- using CERTCLIENTLib;
+using CERTCLIENTLib;
 #endif
 
 namespace RevokeCert
 {
+    public enum RevokeReason
+    {
+        CRL_REASON_UNSPECIFIED = 0,
+        CRL_REASON_KEY_COMPROMISE = 1,
+        CRL_REASON_CA_COMPROMISE = 2,
+        CRL_REASON_AFFILIATION_CHANGED = 3,
+        CRL_REASON_SUPERSEDED = 4,
+        CRL_REASON_CESSATION_OF_OPERATION = 5,
+        CRL_REASON_CERTIFICATE_HOLD = 6
+    }
+
     class Program
     {
+        private const int CC_UIPICKCONFIG = 0x1;
+
         static int Main(string[] args)
         {
-            if (args.Length < 3)
+            string caConfig;
+            int reason;
+            string serial;
+
+            if (args.Length == 1)
             {
+                CCertConfig objCertConfig = new CCertConfig();
+                caConfig = objCertConfig.GetConfig(CC_UIPICKCONFIG);
+
+                reason = (int)RevokeReason.CRL_REASON_CESSATION_OF_OPERATION;
+                serial = args[0];
+            }
+            else if (args.Length == 3)
+            {
+                caConfig = args[0];
+                reason = int.Parse(args[1]);
+                serial = args[2];
+            }
+            else
+            {
+                Console.WriteLine("Usage: RevokeCert.exe [SerialNumber]");
                 Console.WriteLine("Usage: RevokeCert.exe [CAConfig] [Reason] [SerialNumber]");
                 return 2;
             }
-
-            string caConfig = args[0];
-            int reason = int.Parse(args[1]);
-            string serial = args[2];
 
             CCertAdmin admin = null;
             try
